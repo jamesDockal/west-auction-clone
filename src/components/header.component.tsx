dcc2eb9b5be5b9f2b2917d16bfb63eb296c0c58d
J Dockal <jamesdockal@gmail.com>
2025-03-09 18:45:43 -0300

    feat: create search bar focused layout

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,17 +1,26 @@
 import { Lock } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Search } from "./search.component";
 
 export const Header: React.FC = () => {
+  const [isSearchBarFocused, setIsSearchBarFocused] = useState(false);
+
   return (
     <div
-      className="flex justify-around items-center bg-white text-black"
+      className="flex justify-between items-center bg-white text-black"
       style={{
         height: 50,
+        padding: "0px 30px",
       }}
     >
-      <div>
+      <div
+        className="flex justify-center items-center"
+        style={{
+          width: 238,
+          height: 50,
+        }}
+      >
         <Image
           className="dark:invert"
           src="/logo.png"
@@ -23,16 +32,31 @@ export const Header: React.FC = () => {
       </div>
 
       <div
-        className="flex items-center"
+        className="flex items-center w-[659px]"
         style={{
           gap: 30,
         }}
       >
-        <Search />
-        <a className="font-semibold">Online Auctions</a>
-        <a className="cursor-pointer font-semibold text-primary">How to Sell</a>
-        <a className="cursor-pointer font-semibold text-primary">How to Bid</a>
-        <a className="cursor-pointer font-semibold text-primary">Appraisals</a>
+        <Search isFocusedCallback={setIsSearchBarFocused} />
+        <div
+          className={`flex items-center transition-all duration-300 ${
+            isSearchBarFocused && "w-0 opacity-0"
+          }`}
+          style={{
+            gap: 30,
+          }}
+        >
+          <a className="font-semibold whitespace-nowrap">Online Auctions</a>
+          <a className="cursor-pointer font-semibold text-primary whitespace-nowrap">
+            How to Sell
+          </a>
+          <a className="cursor-pointer font-semibold text-primary whitespace-nowrap">
+            How to Bid
+          </a>
+          <a className="cursor-pointer font-semibold text-primary">
+            Appraisals
+          </a>
+        </div>
       </div>
 
       <div
--- src/components/search.component.tsx
+++ src/components/search.component.tsx
@@ -1,14 +1,36 @@
-import { Search as SearchIcon } from "lucide-react";
-import React from "react";
+import { CircleX, Search as SearchIcon } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
-export const Search: React.FC = () => {
+interface Props {
+  isFocusedCallback: (value: boolean) => void;
+}
+
+export const Search: React.FC<Props> = ({ isFocusedCallback }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  useEffect(() => {
+    isFocusedCallback(isFocused);
+  }, [isFocused, isFocusedCallback]);
+
   return (
-    <>
-      <SearchIcon className="absolute text-gray-2 w-[24px] h-[24px] ml-[12px]" />
+    <div className="relative">
+      <SearchIcon className="absolute left-[12px] top-1/2 transform -translate-y-1/2 text-gray-2 w-[24px] h-[24px]" />
       <input
-        className="w-[130px] h-[44px] rounded-[30px] border-2 flex cursor-pointer pl-[52px] font-semibold placeholder-gray-2 pt-[1px]"
-        placeholder="Search"
+        className={`
+          transition-all duration-300
+          ${isFocused ? "w-[640px]" : "w-[130px]"}
+          font-semibold placeholder-gray-2 pt-[1px] rounded-[30px] border-2 cursor-pointer pl-[52px] h-[44px]
+        `}
+        placeholder={isFocused ? "What are you looking for?" : "Search"}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
-    </>
+      {isFocused && (
+        <CircleX className="absolute right-[12px] top-1/2 transform -translate-y-1/2 text-gray-2 w-[24px] h-[24px] " />
+      )}
+    </div>
   );
 };
