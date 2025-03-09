97af0d5779bc0b03a68ac8052dc520846a282a2f
J Dockal <jamesdockal@gmail.com>
2025-03-09 10:10:52 -0300

    feat: setup tailwind and start header component

---
+++ src/components/header.component.tsx
@@ -0,0 +1,58 @@
+import { Lock } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <div
+      className="flex justify-around items-center bg-white text-black"
+      style={{
+        height: 50,
+      }}
+    >
+      <div>
+        <Image
+          className="dark:invert"
+          src="/logo.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </div>
+
+      <div
+        className="flex"
+        style={{
+          gap: 30,
+        }}
+      >
+        search
+        <a className="font-semibold">Online Auctions</a>
+        <a className="font-semibold text-primary">How to Sell</a>
+        <a className="font-semibold text-primary">How to Bid</a>
+        <a className="font-semibold text-primary">Appraisals</a>
+      </div>
+
+      <div
+        className="flex items-center"
+        style={{
+          gap: 30,
+        }}
+      >
+        <div
+          className="flex items-center"
+          style={{
+            gap: 10,
+          }}
+        >
+          <div className="flex items-center justify-center w-8 h-8 gap-2.5 rounded-[20px] p-2 bg-gray-3">
+            <Lock className="text-gray" />
+          </div>
+          <a className="font-semibold text-primary">Login/Register</a>
+        </div>
+        <a className="font-semibold text-primary">Contact</a>
+      </div>
+    </div>
+  );
+};
